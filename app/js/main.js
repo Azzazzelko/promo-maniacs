409d97358606ccd28370f79fdf3922dfe8985582
Vladislav <azzazzelko@gmail.com>
2016-12-23 03:03:21 +0300

    added creation img tab for details page

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,13 @@ $(function() {
   /* ----- custom select ------*/
   $('select').select2();
 
+  /* ----- slider card --------*/
+  var $thumbs = $(".slider-thumbs__list .slider-thumbs__item");
+
+  $thumbs.on("click", function(e){
+  	var $this = this;
+  	$($this).addClass('active').siblings().removeClass('active');
+  });
+
+
 }());
